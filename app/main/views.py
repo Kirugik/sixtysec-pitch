68af92ab4aa3d62a1375ed2ec2d82b55359726cb
Robert Kirui <kirugik79@gmail.com>
2022-05-07 18:10:18 +0300

    Feat: Create templates and render them to view functions

--- app/main/views.py
+++ app/main/views.py
@@ -5,31 +5,37 @@ from flask import render_template
 # views
 @main.route('/')
 def index():
-    pass 
+    return render_template('index.html')
 
 
 @main.route('/pitch')
 def pitch():
-    pass 
+    return render_template('pitches.html')
 
 
 @main.route('/new_pitch')
 def new_pitch():
-    pass
+    return render_template('new_pitch.html')
 
-@main.route('/comments/<int:pitch_id>')
-def comments(pitch_id):
-    pass 
+
+@main.route('/comment/<int:pitch_id>')
+def comment(pitch_id):
+    return render_template('comments.html')
 
 
 @main.route('/user/<name>')
 def user_profile(name):
-    pass 
+    return render_template('profile/user_profile.html')
 
 
 @main.route('/user/<name>/update_profile')
 def update_user_profile(name): 
-    pass 
+    return render_template('profile/update_profile.html')
+
+
+@main.route('/user/<name>/update/pic')
+def update_pic(name): 
+    return render_template('main.user_profile')
 
 
 @main.route('/upvote/<int:id>')
