b6c9ae76a9fe991132c5f12c04e53ec4343a2d61
Robert Kirui <kirugik79@gmail.com>
2022-05-08 15:01:38 +0300

    Feat: Create server configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,57 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail 
 
 
-# creating db instance 
-db = SQLAlchemy() 
+# creating db instance        
+db = SQLAlchemy()
 
+# creating bootstrap instance
+bootstrap = Bootstrap() 
 
-# creating login_manager instance 
+# creating login_manager instance
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+login_manager.login_view = 'auth.login'
+
+# creating photos instance
+photos = UploadSet('photos',IMAGES)
+
+# creating mail instance 
+mail = Mail()
 
 
 def create_app(config_name):
-    
+
     # creating Flask app instance 
     app = Flask(__name__) 
     
     
     # Initializing flask extensions
     db.init_app(app) 
-
-
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name]) 
+    
+    
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    
+    # configure UploadSet
+    configure_uploads(app,photos) 
 
 
     return app 
--- manage.py
+++ manage.py
@@ -1,21 +1,31 @@
-from app import create_app
-from flask_script import Manager, Server 
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
+from  flask_migrate import Migrate, MigrateCommand 
+
 
 # Creating app instance
 app = create_app('development') 
 
-
-
-# Instantiate Manager class by passing in the app instance
+# Instantiate Manager class by passing in the app instance 
 manager = Manager(app)
 
 
-manager.add_command('server',Server)  
+migrate = Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
+
+
+# Create a command line argument to tell us how to run our application
+# Then, use the add_command method to create a new command 'server' which will launch the app server 
+manager.add_command('server',Server)
+
+
 
 @manager.shell 
 def make_shell_context():
-    return dict(app = app) 
+    return dict(app = app,db = db,User = User) 
 
 # Calling the run method on the Manager instance(manager) to run the application 
 if __name__ == '__main__': 
-    manager.run()  
+    manager.run() 
