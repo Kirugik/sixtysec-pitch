b47f762670815a65b6b8c3956f1ef06cc6e02418
Robert Kirui <kirugik79@gmail.com>
2022-05-08 10:04:39 +0300

    Feat: Create sign up and login for user

---
+++ app/auth/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import InputRequired,Email,EqualTo
+from wtforms import ValidationError 
+from ..models import User
+
+
+class SignupForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    username = StringField('Enter your username',validators = [InputRequired()])
+    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
+    submit = SubmitField('Sign Up') 
+    
+    
+    def validate_email(self,data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('The email has already been used.') 
+            
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    password = PasswordField('Password',validators =[InputRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')  
