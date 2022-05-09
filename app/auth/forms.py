7e52686109aa9b08f279de44b3f334024403a5f1
Robert Kirui <kirugik79@gmail.com>
2022-05-09 16:21:28 +0300

    Fix: Fix navbar, sign up, and login pages

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,7 +9,7 @@ class SignupForm(FlaskForm):
     email = StringField('Your Email Address',validators=[InputRequired(),Email()])
     username = StringField('Enter your username',validators = [InputRequired()])
     password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
+    password_confirm = PasswordField('Confirm Password',validators = [InputRequired()])
     submit = SubmitField('Sign Up') 
     
     
