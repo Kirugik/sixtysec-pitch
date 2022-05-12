0b7b5eda19b24a25c0eac115705f40b0901af415
Robert Kirui <kirugik79@gmail.com>
2022-05-12 11:56:48 +0300

    untrack start.sh file

--- manage.py
+++ manage.py
@@ -1,29 +1,29 @@
 from app import create_app,db
-# from flask_script import Manager,Server
+from flask_script import Manager,Server
 from app.models import User
 from  flask_migrate import Migrate, MigrateCommand 
 
 
 # Creating app instance
-app = create_app('development')   
+app = create_app('production')   
 
 # Instantiate Manager class by passing in the app instance 
-# manager = Manager(app)
+manager = Manager(app)
 
 migrate = Migrate(app,db)
 
-# manager.add_command('db',MigrateCommand)
+manager.add_command('db',MigrateCommand)
 
 
 # Create a command line argument to tell us how to run our application
 # Then, use the add_command method to create a new command 'server' which will launch the app server 
-# manager.add_command('server',Server)
+manager.add_command('server',Server)
 
 
 
-# @manager.shell 
-# def make_shell_context():
-#     return dict(app = app,db = db,User = User) 
+@manager.shell 
+def make_shell_context():
+    return dict(app = app,db = db,User = User) 
 
 # Calling the run method on the Manager instance(manager) to run the application 
 if __name__ == '__main__': 
--- start.sh
+++
@@ -1,7 +0,0 @@
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://robert:Kirugik"79@localhost/pitching'
-
-export MAIL_USERNAME='robert' 
-export MAIL_PASSWORD='Kirugik"79'
-export SECRET_KEY='zy5389e!*os92j5@d0wKq' 
-
-python3.8 manage.py server 
